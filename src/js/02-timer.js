875857c6d2e24f144b99036d18f4554c5c861503
Anastasiia Lytviak <lav21051991@gmail.com>
2022-06-05 23:38:34 +0300

    start doing task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,66 @@
-const options = {
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+require("flatpickr/dist/themes/material_red.css");
+
+const refs = {
+    input: document.querySelector('#datetime-picker'),
+    startBtn: document.querySelector('[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]'),
+    wrap: document.querySelector('.timer')
+}
+console.log(refs.input);
+console.log(refs.startBtn);
+
+flatpickr("#datetime-picker", {
     enableTime: true,
+    dateFormat: "F j, Y H:i",
     time_24hr: true,
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
         console.log(selectedDates[0]);
     },
-};
+});
+// const timer = (targetData) => {
+//     setInterval(() => {
+//         const delta = new Date(targetData) - new Date;
+//         return delta;
+//     }, 1000);
+
+
+// }
+
+refs.startBtn.addEventListener("click", startTimer)
+
+function startTimer() {
+
+    // console.log(refs.input.value);
+    //     console.log(new Date());
+    // return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+}
