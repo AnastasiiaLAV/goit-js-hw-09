a3905e52e784aec10af366487c34493a6e24bb78
Anastasiia Lytviak <lav21051991@gmail.com>
2022-06-05 16:58:40 +0300

    add 1 task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+    start: document.querySelector('button[data-start]'),
+    stop: document.querySelector('button[data-stop]')
+}
 
+let intervalId = null;
+
+refs.start.addEventListener("click", () => {
+    intervalId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    refs.stop.disabled = false;
+    refs.start.disabled = true;
+});
+
+refs.stop.addEventListener("click", () => {
+    clearInterval(intervalId);
+    refs.start.disabled = false;
+    refs.stop.disabled = true;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
