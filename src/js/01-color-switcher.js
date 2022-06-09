c74c7029ecedf4fd79d5985a90c1c196a868ec9b
Anastasiia Lytviak <lav21051991@gmail.com>
2022-06-09 16:44:49 +0300

    fixed 1 task and add style

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,8 +2,10 @@ const refs = {
     start: document.querySelector('button[data-start]'),
     stop: document.querySelector('button[data-stop]')
 }
-refs.stop.setAttribute('disabled', 'disabled');
+
 let intervalId = null;
+
+refs.stop.disabled = true;
 
 refs.start.addEventListener("click", () => {
     intervalId = setInterval(() => {
@@ -15,8 +17,8 @@ refs.start.addEventListener("click", () => {
 
 refs.stop.addEventListener("click", () => {
     clearInterval(intervalId);
-    refs.start.disabled = false;
     refs.stop.disabled = true;
+    refs.start.disabled = false;
 });
 
 function getRandomHexColor() {
